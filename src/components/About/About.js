15d3d5e7e6f486e30424009206990250ef4367ff
Himanshu-Saini-1 <himanshu.dcrust@gmai.com>
2024-09-01 11:24:16 +0530

    removed mobile no and email addres

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,16 @@ function About() {
   return (
     <div className="about-container">
       <div className="intro">
-        Hello! I’m Himanshu Saini, a Computer Science student and data analysis
+        Hello! I’m Himanshu Saini, a dedicated Computer Science student with a
+        deep passion for data analysis and problem-solving. Throughout my
+        academic journey, I've had the opportunity to work on various machine
+        learning models, where I tackled complex problems that required
+        innovative thinking and a strategic approach. My work in this area has
+        not only enhanced my technical skills but also fueled my determination
+        to push the boundaries of what's achievable. I thrive on challenges and
+        am constantly driven by the desire to apply my knowledge to real-world
+        scenarios, where I can make a meaningful impact.
+        {/* Hello! I’m Himanshu Saini, a Computer Science student and data analysis
         enthusiast. My academic background has provided me with strong
         programming and statistical skills, fostering critical thinking and
         problem-solving abilities. I've contributed to data-driven
@@ -13,7 +22,7 @@ function About() {
         actionable insights and presenting clear findings to stakeholders for
         impactful business improvements. I am passionate about data science and
         machine learning, aiming to stay at the forefront of technological
-        advancements and drive significant progress in data analysis.
+        advancements and drive significant progress in data analysis. */}
       </div>
     </div>
   );
